1766e96a2c3a8a50eaea1d7390e3fef39cbf9375
Luiz Gonzaga dos Santos Filho <lfilho@gmail.com>
2013-10-30 23:25:04 +0100

    Adding examples/javascript file

---
+++ examples/javascript.js
@@ -0,0 +1,39 @@
+define([
+    'src/my/module1',
+    'src/my/module2',
+    'src/my/module3'
+], function(
+    Module1,
+    Module2,
+    Module3
+){
+
+    var testOne,
+        testTwo,
+        testThree = 3;
+
+    return {
+        method: function () {
+            this.testFour = 1;
+            this.testFive = 2
+            testSix = 2;
+            anotherMethod(function() { /*just wrote here*/ })
+        },
+
+        testSeven: 'foo',
+
+        testEight: {
+            foo: 'bar',
+            bar: 'foo'
+            foo: 'bar',
+        },
+
+        testNine: [
+            1,
+            2
+            3
+        ],
+
+        testTen: null,
+    };
+});
